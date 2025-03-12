8504e0c9103b75561c15536cac2c31a439f1923f
Jabrayilzade77 <yusif.jabrayilzade321@gmail.com>
2025-03-12 14:59:13 +0400

    2D Array problem solved!

---
+++ day6Examples/Array2DApp.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Array2DApp {
+    public static int maxHourglassSum(int[][] arr) {
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int hourglassSum = (
+                        arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                                    arr[i + 1][j + 1] +
+                        arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
+                );
+
+                maxSum = Math.max(maxSum, hourglassSum);
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().split(" ");
+
+            for (int j = 0; j < 6; j++) {
+                arr[i][j] = Integer.parseInt(arrRowTempItems[j]);
+            }
+        }
+
+        bufferedReader.close();
+
+        int result = maxHourglassSum(arr);
+        System.out.println(result);
+    }
+}
